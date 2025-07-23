3d293ade27a9f26b85b927f56548af0f5d50bc7d
Priyanshu Agrawal <priyanshuagrawal@outlook.com>
2025-07-23 18:22:10 +0530

    Clean Repo

--- samples/bring-your-own-code/react/credit-cards-website/src/utils/AppStyleUtils.ts
+++
@@ -1,8 +0,0 @@
-export const containerStyle = "container mx-auto my-4 p-6 bg-white rounded-lg shadow-md";
-export const sectionHeadingStyle = "text-3xl font-bold text-center mb-6 text-gray-800";
-export const formContainerStyle = "max-w-xl mx-auto p-6 bg-white shadow-xl rounded-2xl";
-export const progressBarStyle = "relative w-full bg-gray-200 rounded-full h-2 mb-6";
-export const inputStyle = "w-full border p-2 rounded-lg focus:border-blue-500 focus:outline-none transition-all";
-export const buttonStyle = "bg-blue-600 text-white py-2 px-4 rounded-lg hover:bg-blue-700 transition-all";
-export const italicLargeTextStyle = "text-2xl italic";
-export const heroTextStyle = "text-6xl italic font-extrabold";
